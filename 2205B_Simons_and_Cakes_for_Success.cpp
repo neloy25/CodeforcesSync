e40142619ba1d08748aeef09265da54a086b55a2
Sadman Safin Neloy <sadmansafinneloy678@gmail.com>
2026-07-23 02:16:50 +0600

    Sync Codeforces: 2205B - Simons and Cakes for Success [C++23 (GCC 14-64, msys2)]

--- 2205B_Simons_and_Cakes_for_Success.cpp
+++ 2205B_Simons_and_Cakes_for_Success.cpp
@@ -4,32 +4,39 @@ using namespace std;
 #define ll long long
 #define nl << endl;
  
+ll MOD;
+ll pow(ll a, ll b) {
+    ll res = 1;
+    while(b != 0) {
+        if(b & 1) {
+            res = (res * a) % MOD;
+        }
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
 void solve() {
     ll n;
-    cin >>n;
+    cin >> n;
  
-    set<ll> primes;
-    while(true) {
-        bool flag = true;
-        for(int i = 2; i * i <= n;i++) {
-            if(n % i == 0) {
-                primes.insert(i);
-                n /= i;
-                flag = false;
-                break;
-            }
-        }
- 
-        if(flag) {
-            primes.insert(n);
-            break;
+    MOD = n;
+    vector<ll> div;
+    for(int i = 1; i * i <= n; i++) {
+        if(n % i == 0) {
+            div.push_back(i);
+            if(i != n / i) div.push_back(n / i);
         }
     }
-    ll res = 1;
-    for(auto val : primes) {
-        res *= val;
+ 
+    sort(div.begin(), div.end());
+    for(int i = 0; i < div.size(); i++) {
+        if(pow(div[i], n) % n == 0) {
+            cout << div[i] << endl;
+            return;
+        }
     }
-    cout <<res << endl;
 }
 int main() {
     ios_base::sync_with_stdio(false);
